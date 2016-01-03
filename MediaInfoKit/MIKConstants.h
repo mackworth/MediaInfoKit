bf50ec3ccf9ee05a290c627d45ed39e1b0d06dff
Jeremy Vizzini <contact@jeremyvizzini.com>
2016-01-03 19:32:40 +0100

    Add some constant keys

---
+++ MediaInfoKit/MIKConstants.h
@@ -0,0 +1,68 @@
+//
+//  MIKConstants.h
+//  MediaInfoKit
+//
+//  Created by Jeremy Vizzini.
+//  This software is released subject to licensing conditions as detailed in LICENCE.md
+//
+
+#import <Foundation/Foundation.h>
+
+#define MIK_EXPORT FOUNDATION_EXPORT
+
+NS_ASSUME_NONNULL_BEGIN
+
+// The english keys of the streams
+MIK_EXPORT NSString * const MIKGeneralStreamKey;
+MIK_EXPORT NSString * const MIKVideoStreamKey;
+MIK_EXPORT NSString * const MIKAudioStreamKey;
+MIK_EXPORT NSString * const MIKAudio1StreamKey;
+MIK_EXPORT NSString * const MIKAudio2StreamKey;
+
+// The english keys of the values
+MIK_EXPORT NSString *const MIKCompleteNameKey;
+MIK_EXPORT NSString *const MIKFormatKey;
+MIK_EXPORT NSString *const MIKFormatProfileKey;
+MIK_EXPORT NSString *const MIKCodecIDKey;
+MIK_EXPORT NSString *const MIKFileSizeKey;
+MIK_EXPORT NSString *const MIKDurationKey;
+MIK_EXPORT NSString *const MIKOverallBitRateMode;
+MIK_EXPORT NSString *const MIKOverallBitRateKey;
+MIK_EXPORT NSString *const MIKMovieNameKey;
+MIK_EXPORT NSString *const MIKEncodedDateKey;
+MIK_EXPORT NSString *const MIKTaggedDateKey;
+MIK_EXPORT NSString *const MIKCoverKey;
+MIK_EXPORT NSString *const MIKShortDescKey;
+MIK_EXPORT NSString *const MIKLongDescKey;
+MIK_EXPORT NSString *const MIKKeyWordsKey;
+MIK_EXPORT NSString *const MIKStikKey;
+MIK_EXPORT NSString *const MIKiTunMOVIKey;
+MIK_EXPORT NSString *const MIKIDKey;
+MIK_EXPORT NSString *const MIKFormatInfoKey;
+MIK_EXPORT NSString *const MIKFormatSettingsCABACKey;
+MIK_EXPORT NSString *const MIKFormatSettingsReFramesKey;
+MIK_EXPORT NSString *const MIKCodecIDInfoKey;
+MIK_EXPORT NSString *const MIKBitRateKey;
+MIK_EXPORT NSString *const MIKMaximumBitRateKey;
+MIK_EXPORT NSString *const MIKWidthKey;
+MIK_EXPORT NSString *const MIKHeightKey;
+MIK_EXPORT NSString *const MIKDisplayAspectRatioKey;
+MIK_EXPORT NSString *const MIKFrameRateModeKey;
+MIK_EXPORT NSString *const MIKFrameRateKey;
+MIK_EXPORT NSString *const MIKColorSpaceKey;
+MIK_EXPORT NSString *const MIKChromaSubsamplingKey;
+MIK_EXPORT NSString *const MIKBitRateModeKey;
+MIK_EXPORT NSString *const MIKBitDepthKey;
+MIK_EXPORT NSString *const MIKScanTypeKey;
+MIK_EXPORT NSString *const MIKBitsPixelFrameKey;
+MIK_EXPORT NSString *const MIKStreamSizeKey;
+MIK_EXPORT NSString *const MIKTitleKey;
+MIK_EXPORT NSString *const MIKWritingLibraryKey;
+MIK_EXPORT NSString *const MIKEncodingSettingsKey;
+MIK_EXPORT NSString *const MIKLanguageKey;
+MIK_EXPORT NSString *const MIKChannelsKey;
+MIK_EXPORT NSString *const MIKChannelPositionsKey;
+MIK_EXPORT NSString *const MIKSamplingRateKey;
+MIK_EXPORT NSString *const MIKCompressionModeKey;
+
+NS_ASSUME_NONNULL_END
---
+++ MediaInfoKit/MIKConstants.m
@@ -0,0 +1,59 @@
+//
+//  MIKConstants.m
+//  MediaInfoKit
+//
+//  Created by Jeremy Vizzini.
+//  This software is released subject to licensing conditions as detailed in LICENCE.md
+//
+
+#import "MIKConstants.h"
+
+NSString *const MIKGeneralStreamKey          = @"General";
+NSString *const MIKVideoStreamKey            = @"Video";
+NSString *const MIKAudioStreamKey            = @"Audio";
+NSString *const MIKAudio1StreamKey           = @"Audio #1";
+NSString *const MIKAudio2StreamKey           = @"Audio #2";
+
+NSString *const MIKCompleteNameKey           = @"Complete name";
+NSString *const MIKFormatKey                 = @"Format";
+NSString *const MIKFormatProfileKey          = @"Format profile";
+NSString *const MIKCodecIDKey                = @"Codec ID";
+NSString *const MIKFileSizeKey               = @"File size";
+NSString *const MIKDurationKey               = @"Duration";
+NSString *const MIKOverallBitRateModeKey     = @"Overall bit rate mode";
+NSString *const MIKOverallBitRateKey         = @"Overall bit rate";
+NSString *const MIKMovieNameKey              = @"Movie name";
+NSString *const MIKEncodedDateKey            = @"Encoded date";
+NSString *const MIKTaggedDateKey             = @"Tagged date";
+NSString *const MIKCoverKey                  = @"Cover";
+NSString *const MIKShortDescKey              = @"desc";
+NSString *const MIKLongDescKey               = @"ldes";
+NSString *const MIKKeyWordsKey               = @"keyw";
+NSString *const MIKStikKey                   = @"stik";
+NSString *const MIKiTunMOVIKey               = @"iTunMOVI";
+NSString *const MIKIDKey                     = @"ID";
+NSString *const MIKFormatInfoKey             = @"Format/Info";
+NSString *const MIKFormatSettingsCABACKey    = @"Format settings, CABAC";
+NSString *const MIKFormatSettingsReFramesKey = @"Format settings, ReFrames";
+NSString *const MIKCodecIDInfoKey            = @"Codec ID/Info";
+NSString *const MIKBitRateKey                = @"Bit rate";
+NSString *const MIKMaximumBitRateKey         = @"Maximum bit rate";
+NSString *const MIKWidthKey                  = @"Width";
+NSString *const MIKHeightKey                 = @"Height";
+NSString *const MIKDisplayAspectRatioKey     = @"Display aspect ratio";
+NSString *const MIKFrameRateModeKey          = @"Frame rate mode";
+NSString *const MIKFrameRateKey              = @"Frame rate";
+NSString *const MIKColorSpaceKey             = @"Color space";
+NSString *const MIKChromaSubsamplingKey      = @"Chroma subsampling";
+NSString *const MIKBitRateModeKey            = @"Bit rate mode";
+NSString *const MIKBitDepthKey               = @"Bit depth";
+NSString *const MIKScanTypeKey               = @"Scan type";
+NSString *const MIKBitsPixelFrameKey         = @"Bits/(Pixel*Frame)";
+NSString *const MIKStreamSizeKey             = @"Stream size";
+NSString *const MIKTitleKey                  = @"Title";
+NSString *const MIKWritingLibraryKey         = @"Writing library";
+NSString *const MIKLanguageKey               = @"Language";
+NSString *const MIKChannelsKey               = @"Channel(s)";
+NSString *const MIKChannelPositionsKey       = @"Channel positions";
+NSString *const MIKSamplingRateKey           = @"Sampling rate";
+NSString *const MIKCompressionModeKey        = @"Compression mode";
